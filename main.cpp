ff1bc4fa29226b1ac4f388ad5e07d26ef6ed35be
SMahdi <S.Mahdi.Maddah@gmail.com>
2023-12-29 15:45:17 +0330

    add color and score board

--- main.cpp
+++ main.cpp
@@ -2,16 +2,52 @@
 // product by Seyyed Mahdi Maddah
 
 #include <bits/stdc++.h>
+#include <windows.h>
 #include<conio.h>
-
 using namespace std;
+
+#define setBrown setTextColor(6, 0)
+#define setBlue setTextColor(1, 0)
+#define setWhite setTextColor(15, 0)
+#define setRed setTextColor(4, 0)
+
+void start_game();
+
+
 const int max_tree_size = 20;
 int tree[max_tree_size];
 int player = 1;
 int& base = tree[10]; // A place where players and branches are checked (tree 10)
+int max_score = 0;
+int score = 0;
+
+void setTextColor(int textColor, int backColor) {
+  HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+  int colorAttribute = backColor << 4 | textColor;
+  SetConsoleTextAttribute(consoleHandle, colorAttribute);
+}
+
+
+
+
+
+
+void show_score_board(){
+	setRed;
+	cout << "\n";
+	cout << "\t _______________________________\n";
+	cout << "\t|Score: " <<setw(7)<< score << " |  Max = " << setw(7) << max_score << "|\n";
+	cout << "\t|_______________|_______________|\n";
+}
+void update_score(){
+	score += 30;
+	max_score = max(score, max_score);
+}
 void display(){
 	system("cls");
+	setBrown;
 	cout << "\t _______________________________\n";
+	setBrown;
 	for (int i = 1; i <= 9; i++){
 		if (tree[i] == 1){
 			cout << "\t|\t     |||||||     \t|\n";
@@ -24,19 +60,29 @@ void display(){
 		}
 	}
 	if (player == 1){
+		cout << "\t|\t";
+		setBlue;
+		cout << "  #  ";
+		setBrown;
 		if (base == 1){
-			cout << "\t|\t  #  |||||||     \t|\n";
+			cout << "|||||||     \t|\n";
 		}else if (base == 2){
-			cout << "\t|\t  #  |||||||=====\t|\n";
+			cout << "|||||||=====\t|\n";
 		}
 	}else if (player == 2){
 		if (base == 1){
-			cout << "\t|\t     |||||||  #  \t|\n";
+			cout << "\t|\t     |||||||";
 		}else if (base == 3){
-			cout << "\t|\t=====|||||||  #  \t|\n";
+			cout << "\t|\t=====|||||||";
 		}
+		setBlue;
+		cout << "  #  ";
+		setBrown;
+		cout << "\t|\n";
 	}
 	cout << "\t|_______________________________|\n";
+//	setRed;
+	show_score_board();
 }
 void game_over(){
 	cout << "\tGame Over\n";
@@ -50,10 +96,13 @@ void check_encounter(){
 	}
 }
 void move(){
-	char c = getch();
-	if (c == 'a' || c == 'A')player = 1;
-	else if (c == 'd' || c == 'D')player = 2;
-	else move();
+	int c = getch();
+	if (c == 224){
+		c = getch();
+		if (c == 75)player = 1; // left
+		else if (c == 77)player = 2; // right
+		else move();
+	}else move();
 }
 void update_tree(){
 	// tree 1 equal to tree trunk
@@ -63,19 +112,32 @@ void update_tree(){
 		tree[i] = tree[i-1];
 	}
 	tree[1] = (rand()%3) + 1;
+	if ((tree[1] == 2 && tree[2] == 3) || (tree[1] == 3 && tree[2] == 2)){
+		tree[1] = 1;
+	}
 }
 void make_tree(){
 	// tree 1 equal to tree trunk
-	for (int i = 1; i <= 10; i++){
-		tree[i] = 1;
+	for (int i = 1; i < 10; i++){
+		tree[i] = (rand()%3) + 1;
+		if ((tree[i] == 2 && tree[i-1] == 3) || (tree[i] == 3 && tree[i-1] == 2)){
+			tree[i] = 1;
+		}
+	}
+	tree[10] = 1;
+}
+void start_game(){
+	make_tree();
+	score = 0;
+	while (true){
+		display();
+		move();
+		check_encounter();
+		update_tree();
+		check_encounter();
+		update_score();
 	}
 }
 int main(){
-	make_tree();
-	while (true){
-		move();
-		update_tree();
-		display();
-		check_encounter();
-	}
+	start_game(); 
 }
